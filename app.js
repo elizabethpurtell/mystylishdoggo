3c58e811053b2620e654e378ac08c5f4ba5f792f
elizabethpurtell <epurtell.CF23@gmail.com>
2023-05-27 19:09:18 -0700

    added js functionality to update for class06, 07, updated css to match

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 
+// 
 
-
-// document.write("showing where the script is running");
-// document.write("showing where the script is running");
-// document.write("showing where the script is running");
-// document.write("showing where the script is running");
+//
 
 
 
+function greetUser(){
+  let name = prompt("What is your name?");
+      if(name == ""){
+      name = prompt("I really want to know your name, please!");
+      }
+      document.write("Hi, " + name + "We are happy to have you! Woof!");
+      return name;
+}
 
+function userChoice(){
+  let choice = prompt("Are you a dog or cat person?");
+  if (choice === "dog") {
+    alert("You have come to the right site!");
+  }
+  else if(choice === "cat") {
+    alert("You should visit our sister site!");
+  }
+}
